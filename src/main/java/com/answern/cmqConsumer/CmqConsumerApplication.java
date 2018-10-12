3ec1145571086d19c509f543acf76014e32d698a
wemlive <674693934@qq.com>
2018-10-12 17:14:11 +0800

    增加初始化项目

---
+++ src/main/java/com/answern/cmqConsumer/CmqConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.answern.cmqConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmqConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmqConsumerApplication.class, args);
+	}
+}
